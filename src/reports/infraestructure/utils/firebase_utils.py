4e2146d1daaeb3d05af8c915f7604b2d1d7198f9
Daniel-Flore2 <danielflores.dev1@gmail.com>
2024-07-28 06:27:07 -0600

    feature requeriments

--- src/reports/infraestructure/utils/firebase_utils.py
+++
@@ -1,8 +0,0 @@
-import firebase_admin
-from firebase_admin import credentials, storage
-
-
-cred = credentials.Certificate('firebase.json')
-firebase_admin.initialize_app(cred, {
-    'storageBucket': 'gs://eco-reporte.appspot.com'
-})
