d3b1c43e3c9eea6a7b27bbd4cf84ebc30e2d9d0f
Daniel-Flore2 <danielflores.dev1@gmail.com>
2024-07-28 12:14:04 -0600

    feature enviroments

--- app.py
+++ app.py
@@ -15,14 +15,27 @@ from src.reports.infraestructure.controllers.statistics_controller import Statis
 from src.reports.infraestructure.routes.report_routes import create_report_blueprint
 from src.reports.infraestructure.routes.statistics_routes import create_statistics_blueprint
 
+firebase_config = {
+    "type": os.getenv('FIREBASE_TYPE'),
+    "project_id": os.getenv('FIREBASE_PROJECT_ID'),
+    "private_key_id": os.getenv('FIREBASE_PRIVATE_KEY_ID'),
+    "private_key": os.getenv('FIREBASE_PRIVATE_KEY').replace('\\n', '\n'),
+    "client_email": os.getenv('FIREBASE_CLIENT_EMAIL'),
+    "client_id": os.getenv('FIREBASE_CLIENT_ID'),
+    "auth_uri": os.getenv('FIREBASE_AUTH_URI'),
+    "token_uri": os.getenv('FIREBASE_TOKEN_URI'),
+    "auth_provider_x509_cert_url": os.getenv('FIREBASE_AUTH_PROVIDER_X509_CERT_URL'),
+    "client_x509_cert_url": os.getenv('FIREBASE_CLIENT_X509_CERT_URL'),
+    "universe_domain": os.getenv('FIREBASE_UNIVERSE_DOMAIN')
+}
+
 # Flask application configuration
 app = Flask(__name__)
 CORS(app) 
 app.config.from_object(Config)
 
 # Initialize Firebase
-firebase_config_path = os.getenv('FIREBASE_CONFIG_PATH', 'src/database/firebase_config.json')
-cred = credentials.Certificate(firebase_config_path)
+cred = credentials.Certificate(firebase_config)
 firebase_admin.initialize_app(cred, {
     'storageBucket': 'eco-reporte.appspot.com'
 })
